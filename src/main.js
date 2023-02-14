ea5a1ea20e3df10d4bd5ae1758d2d52caedeb81c
S-yifan13 <1964098644@qq.com>
2023-02-14 23:30:05 +0800

    axios配置完成

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from './store'
 import Chakra,  { CThemeProvider }  from '@chakra-ui/vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+import axios from "axios";
+// axios.defaults.baseURL = "http://43.138.82.70/api"
+Vue.prototype.$axios = axios;
 Vue.use(ElementUI);
 Vue.use(Chakra)
 
